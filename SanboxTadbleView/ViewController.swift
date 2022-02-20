abdbc977cb2c6ca45d659cdc648232885f78e08d
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-02-20 19:36:56 +0300

    Initial Commit

---
+++ SanboxTadbleView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SanboxTadbleView
+//
+//  Created by Олейник Богдан on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
