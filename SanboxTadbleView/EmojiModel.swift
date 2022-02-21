c3e1f1c2651ee8d3da61b1ec5acded7b28d7fdcf
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-02-21 19:12:14 +0300

    1.0

---
+++ SanboxTadbleView/EmojiModel.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  SanboxTadbleView
+//
+//  Created by Олейник Богдан on 20.02.2022.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
+
---
+++ SanboxTadbleView/EmojiTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  EmijiTableViewCell.swift
+//  SanboxTadbleView
+//
+//  Created by Олейник Богдан on 20.02.2022.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    func set(object: Emoji) {
+        self.emojiLabel.text = object.emoji
+        self.nameLabel.text = object.name
+        self.descriptionLabel.text = object.description
+    }
+    
+}
